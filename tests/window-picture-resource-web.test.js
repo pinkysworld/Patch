87ab28eeb04162c10b484cf7f015142e8408c3f9
pinkysworld <85413447+pinkysworld@users.noreply.github.com>
2026-08-26 15:37:00 +0200

    Test Picture resource resolver through existing webapp module

--- tests/window-picture-resource-web.test.js
+++ tests/window-picture-resource-web.test.js
@@ -1,7 +1,6 @@
 import test from 'node:test';
 import assert from 'node:assert/strict';
-import { buildStandaloneWebApp } from '../src/webapp.js';
-import { pictureResourceDataUri } from '../src/window-web-picture.js';
+import { buildStandaloneWebApp, pictureResourceDataUri } from '../src/webapp.js';
 
 const RESOURCE = Object.freeze({
   id: 'app.logo',
